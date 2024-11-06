aac14cb43c952b667331e6c416c7ed57423c60a3
Ayesha Shakeel <47654149+ayesha-shakeel@users.noreply.github.com>
2024-11-06 13:12:07 -0500

    Update main.java
    Added a main menu function that will be filled in with what we need to ask clients

---
+++ main.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        mainMenu();
+    }
+}
+
+// The main menu
+private static void mainMenu(){
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) { // an infinite loop that will always ask the user for input
+
+    }
+}
