94a58eeda534a5f40054a9e69ab8e459b25af563
Ayesha Shakeel <47654149+ayesha-shakeel@users.noreply.github.com>
2024-11-06 13:33:25 -0500

    Update main.java
    fixed silly bugs (moved content around to make it compile)

--- main.java
+++ main.java
@@ -1,14 +1,26 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        mainMenu();
-    }
+import java.util.*;
+
+public class WasteManagementSystem {
+	public static void main(String[] args) {
+		mainMenu();
+	}
+	//The main menu
+	private static void mainMenu(){
+	 Scanner scanner = new Scanner(System.in);
+
+	 while (true) { // an infinite loop that will always ask the user for input
+	     // Here we will show the user the menu
+	     System.out.println("Hello! Welcome to the Drainage Request Management System!");
+	     System.out.println("Please select from one of the menu options below:");
+	     System.out.println("1. I would like to request information about an exisitng service request.");
+	     System.out.println("2. I would like to file a new service request.");
+	     System.out.println("3. I would like to see drainage requests in a specific area");
+	     System.out.println("4. I would like to update an exisiting drainage request");
+	     System.out.println("5. I would like to exit this menu");
+	     
+	 }
+	}
+
+	
 }
 
-// The main menu
-private static void mainMenu(){
-    Scanner scanner = new Scanner(System.in);
-
-    while (true) { // an infinite loop that will always ask the user for input
-
-    }
-}
