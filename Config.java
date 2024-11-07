3fc4aa4ab8050f988911e980aac28f491e1949f3
Yihan <tina.yihan.xie@gmail.com>
2024-11-06 20:42:21 -0500

    config

--- Config.java
+++ Config.java
@@ -3,7 +3,7 @@ public class Config {
     // Environment, file path settings
     // Need to be set before running the program !!
     // This is for option 3 & option 4
-    public static final String CSV_FILE_PATH_1 = "/Users/xieyihan/COMP3220-Team10/Drainage_YTD.csv";
+    public static final String CSV_FILE_PATH_1 = "";
 
     public static String getCsvFilePath() {
         return CSV_FILE_PATH_1;
