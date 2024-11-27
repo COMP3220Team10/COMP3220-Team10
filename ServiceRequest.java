d8c7f6d317243504264a112129f605f317078898
Violet Wang <87334924+Violet121380@users.noreply.github.com>
2024-11-26 21:12:23 -0500

    Update ServiceRequest.java

--- ServiceRequest.java
+++ ServiceRequest.java
@@ -11,7 +11,10 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.DefaultComboBoxModel;
-
+/**
+* @author WindowBuilder
+* Description: The serviceRequest file is created for the Service Request form submission page
+*/
 public class ServiceRequest {
 
 	private JFrame frame;
@@ -33,41 +36,41 @@ public class ServiceRequest {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+		//A title is created
 		JLabel titleLabel = new JLabel("Service Request");
 		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
 		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleLabel.setBounds(133, 0, 162, 36);
 		frame.getContentPane().add(titleLabel);
-		
+		//Type label is created here
 		JLabel typeLabel = new JLabel("Service Type");
 		typeLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		typeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		typeLabel.setBounds(63, 36, 83, 23);
 		frame.getContentPane().add(typeLabel);
-		
+		//comboBox for types selection
 		JComboBox<String> comboBox = new JComboBox<String>();
 		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"Drainage Service", "Other"}));
 		comboBox.setBounds(140, 60, 155, 22);
 		frame.getContentPane().add(comboBox);
-		
+		//date label is created
 		JLabel dateLabel = new JLabel("Date/Time");
 		dateLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		dateLabel.setBounds(63, 93, 74, 14);
 		frame.getContentPane().add(dateLabel);
-		
+		//text field for date
 		datetextField = new JTextField();
 		datetextField.setBounds(140, 113, 155, 20);
 		frame.getContentPane().add(datetextField);
 		datetextField.setColumns(10);
-		
+		//Address label is created
 		JLabel AddrLabel = new JLabel("Address");
 		AddrLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		AddrLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		AddrLabel.setBounds(63, 144, 74, 14);
 		frame.getContentPane().add(AddrLabel);
-		
+		//text field for address
 		addrtextField_2 = new JTextField();
 		addrtextField_2.setColumns(10);
 		addrtextField_2.setBounds(140, 163, 155, 20);
